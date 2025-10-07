7b554adb27448d2a8457599211ccaf194b72ea25
Aleksandar Joshevski <if24b280@technikum-wien.at>
2025-10-07 19:33:28 +0200

    divisible by 3 is fizz, divisible by 5 is buzz

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -6,4 +6,21 @@ describe('String Calculator', function() {
 	it('should return numbers 1-100', function() {
 		expect(calculator.add('').length).toEqual(100);
 	});
+
+	it('should return every 3rd nr with Fizz', function() {
+		expect(calculator.add('')[2]).toEqual('Fizz');
+		expect(calculator.add('')[5]).toEqual('Fizz');
+		expect(calculator.add('')[26]).toEqual('Fizz');
+		expect(calculator.add('')[56]).toEqual('Fizz');
+		expect(calculator.add('')[98]).toEqual('Fizz');
+	});
+
+	it('should return every 5th nr with Buzz', function() {
+		expect(calculator.add('')[4]).toEqual('Buzz');
+		expect(calculator.add('')[14]).toEqual('Buzz');
+		expect(calculator.add('')[54]).toEqual('Buzz');
+		expect(calculator.add('')[74]).toEqual('Buzz');
+		expect(calculator.add('')[89]).toEqual('Buzz');
+	});
+
 });
--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -3,8 +3,16 @@ function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	let zahlen = [];
 	for(let i = 1; i <= 100; i++){
-		zahlen.push(i);
-        console.log(i);
+		let x = i;
+
+		if(i % 3 === 0){
+			x = 'Fizz';
+		}
+		if(i % 5 === 0){
+			x = 'Buzz';
+		}
+		zahlen.push(x);
+        console.log(x);
     }
 	return zahlen;
 };
