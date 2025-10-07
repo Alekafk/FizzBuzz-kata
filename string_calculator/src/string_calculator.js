e800cce3420514f2af9ea60391c7a3b0bc184878
Aleksandar Joshevski <if24b280@technikum-wien.at>
2025-10-07 19:20:44 +0200

    printing 1-100 now works

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -1,5 +1,10 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
-	
+	let zahlen = [];
+	for(let i = 1; i <= 100; i++){
+		zahlen.push(i);
+        console.log(i);
+    }
+	return zahlen;
 };
